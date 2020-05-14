201ddbb5e386e1cce3df0de42dc68b30dd131f21
lorange <jglorange@gmail.com>
2020-05-14 22:27:25 +0000

    mobile fix

--- src/components/ImageGrid/styled.js
+++ src/components/ImageGrid/styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components"
 
 export const Grid = styled.div`
   display: grid;
-  grid-auto-rows: var(--boxSize);
+  grid-auto-rows: minmax(var(--boxSize), auto);
   gap: var(--smallPad);
   grid-template-columns: repeat( auto-fit, minmax(var(--boxSize), 1fr) );
   background: var(--primary);
